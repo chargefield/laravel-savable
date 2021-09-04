30a8db14c317815cb732ca9b9a774fc042377f61
Clayton D'Mello <c.dmello@vehikl.com>
2021-09-03 21:05:17 -0400

    Refactor boolean tests

--- tests/Feature/BooleanFieldTest.php
+++ tests/Feature/BooleanFieldTest.php
@@ -3,90 +3,95 @@
 namespace Chargefield\Supermodel\Tests\Feature;
 
 use Chargefield\Supermodel\Fields\BooleanField;
-use Chargefield\Supermodel\Fields\Field;
 use Chargefield\Supermodel\Tests\TestCase;
 
 class BooleanFieldTest extends TestCase
 {
     /** @test */
-    public function it_can_make_a_new_string_field_instance()
+    public function it_can_make_a_new_field_instance()
     {
         $this->assertInstanceOf(BooleanField::class, BooleanField::make('is_testing'));
     }
 
     /** @test */
-    public function it_can_set_and_get_true_from_the_boolean_value()
+    public function it_can_set_a_value_true_and_return_true()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value(true));
+        $field->value(true);
         $this->assertTrue($field->handle());
     }
 
     /** @test */
-    public function it_can_set_and_get_true_from_the_string_boolean_value()
+    public function it_can_set_a_string_value_true_and_return_true()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value('true'));
+        $field->value('true');
         $this->assertTrue($field->handle());
     }
 
     /** @test */
-    public function it_can_set_and_get_true_from_the_integer_boolean_value()
+    public function it_can_set_a_string_value_1_and_return_true()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value('1'));
+        $field->value('1');
         $this->assertTrue($field->handle());
     }
 
     /** @test */
-    public function it_can_set_and_get_false_from_the_boolean_value()
+    public function it_can_set_a_value_false_and_return_false()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value(false));
+        $field->value(false);
         $this->assertFalse($field->handle());
     }
 
     /** @test */
-    public function it_can_set_and_get_false_from_the_string_boolean_value()
+    public function it_can_set_a_string_value_false_and_return_false()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value('false'));
+        $field->value('false');
         $this->assertFalse($field->handle());
     }
 
     /** @test */
-    public function it_can_set_and_get_false_from_the_integer_boolean_value()
+    public function it_can_set_a_string_value_0_and_return_false()
     {
         $field = BooleanField::make('is_testing');
-        $this->assertInstanceOf(Field::class, $field->value('0'));
+        $field->value('0');
         $this->assertFalse($field->handle());
     }
 
     /** @test */
-    public function it_returns_false_when_value_is_not_set()
+    public function it_returns_false_when_a_value_is_not_set()
     {
         $field = BooleanField::make('is_testing');
         $this->assertFalse($field->handle());
     }
 
     /** @test */
-    public function it_returns_null_when_value_is_not_set_and_allow_null()
+    public function it_returns_null_when_nullable_and_value_is_not_a_boolean()
     {
-        $field = BooleanField::make('is_testing')->value('not-a-boolean')->nullable();
+        $field = BooleanField::make('is_testing');
+        $field->nullable();
+        $field->value('not-a-boolean');
         $this->assertNull($field->handle());
     }
 
     /** @test */
-    public function it_returns_null_when_value_is_null_and_set_to_nullable()
+    public function it_returns_null_when_nullable_and_value_is_null()
     {
-        $field = BooleanField::make('is_testing')->value(null)->nullable();
+        $field = BooleanField::make('is_testing');
+        $field->nullable();
+        $field->value(null);
         $this->assertNull($field->handle());
     }
 
     /** @test */
-    public function it_returns_null_when_value_is_empty_and_set_to_nullable()
+    public function it_returns_null_when_nullable_and_value_is_empty_string()
     {
-        $field = BooleanField::make('is_testing')->value('')->nullable();
+        $field = BooleanField::make('is_testing');
+        $field->nullable();
+        $field->value('');
         $this->assertNull($field->handle());
     }
 }
